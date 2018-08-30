f7dc998ff9b7e5c17cfad64b5b75ce090cbf1579
None <None>
2018-08-30 00:26:06 -0700

    [Defect]:Test

---
+++ autoscript.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# set n to 1
+n=1
+
+# continue until $n equals 5
+while [ $n -le 10 ]
+do
+	echo "Welcome $n times."
+        wget "https://github.com/Lohit29/ETA_Test/blob/master/src/anon.c"     
+        sleep 10
+	n=$(( n+1 ))	 # increments $n
+done
+git reset --hard
+git pull
+git add autoscript.sh
+git commit -m "[Defect]:Test"
+git push origin --all
+
